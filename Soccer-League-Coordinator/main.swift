4133b85bcf9b3a0d71fd79dfcd8d72d5bb1da863
tdimnet <thomas.dimnet@gmail.com>
2018-04-07 16:17:19 +0200

    Change dictionnary for each player structure and logic.
    - Each player dictionnary now contains only string.
    - Loop through the players array in order to fill a two array: one for experiemented players and the other one for novice.

--- Soccer-League-Coordinator/main.swift
+++ Soccer-League-Coordinator/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-let players: [[String: Any]] = [
+let players: [[String: String]] = [
     joeSmith,
     jillTanner,
     billBon,
@@ -28,3 +28,24 @@ let players: [[String: Any]] = [
     lesClay,
     herschelKrustofski
 ]
+
+var novicePlayers: [[String: String]] = []
+var experimentedPlayers: [[String: String]] = []
+
+for player in players {
+    if player["soccerExperience"] == "Yes" {
+        experimentedPlayers.append(player)
+    } else {
+        novicePlayers.append(player)
+    }
+}
+
+print("The number of experimented players is \(experimentedPlayers.count)")
+print("The number of novice players is \(novicePlayers.count)")
+
+
+
+
+
+
+
--- Soccer-League-Coordinator/players.swift
+++ Soccer-League-Coordinator/players.swift
@@ -8,139 +8,139 @@
 
 import Foundation
 
-let joeSmith: [String: Any] = [
+let joeSmith: [String: String] = [
     "name": "Joe Smith",
-    "height": 42,
-    "soccerExperience": true,
+    "height": "42",
+    "soccerExperience": "Yes",
     "guardian": "Jim and Jan Smith"
 ]
 
-let jillTanner: [String: Any] = [
+let jillTanner: [String: String] = [
     "name": "Jill Tanner",
-    "height": 36,
-    "soccerExperience": true,
+    "height": "36",
+    "soccerExperience": "Yes",
     "guardian": "Clara Tanner"
 ]
 
-let billBon: [String: Any] = [
+let billBon: [String: String] = [
     "name": "Bill Bon",
-    "height": 43,
-    "soccerExperience": true,
+    "height": "43",
+    "soccerExperience": "Yes",
     "guardian": "Sara and Jenny Bon"
 ]
 
-let evaGordon: [String: Any] = [
+let evaGordon: [String: String] = [
     "name": "Eva Gordon",
-    "height": 45,
-    "soccerExperience": false,
+    "height": "45",
+    "soccerExperience": "No",
     "guardian": "Wendy and Mike Gordon"
 ]
 
-let mattGill: [String: Any] = [
+let mattGill: [String: String] = [
     "name": "Matt Gill",
-    "height": 40,
-    "soccerExperience": false,
+    "height": "40",
+    "soccerExperience": "No",
     "guardian": "Charles and Sylvia Gill"
 ]
 
-let kimmyStein: [String: Any] = [
+let kimmyStein: [String: String] = [
     "name": "Kimmy Stein",
-    "height": 41,
-    "soccerExperience": false,
+    "height": "41",
+    "soccerExperience": "No",
     "guardian": "Bill and Hillary Stein"
 ]
 
-let sammyAdams: [String: Any] = [
+let sammyAdams: [String: String] = [
     "name": "Sammy Adams",
-    "height": 45,
-    "soccerExperience": false,
+    "height": "45",
+    "soccerExperience": "No",
     "guardian": "Jeff Adams"
 ]
 
-let karlSaygan: [String: Any] = [
+let karlSaygan: [String: String] = [
     "name": "Karl Saygan",
-    "height": 42,
-    "soccerExperience": true,
+    "height": "42",
+    "soccerExperience": "Yes",
     "guardian": "Heather Bledsoe"
 ]
 
 
-let suzaneGreenberg: [String: Any] = [
+let suzaneGreenberg: [String: String] = [
     "name": "Suzane Greenberg",
-    "height": 44,
-    "soccerExperience": true,
+    "height": "44",
+    "soccerExperience": "Yes",
     "guardian": "Henrietta Dumas"
 ]
 
 
-let salDali: [String: Any] = [
+let salDali: [String: String] = [
     "name": "Sal Dali",
-    "height": 41,
-    "soccerExperience": false,
+    "height": "41",
+    "soccerExperience": "No",
     "guardian": "Gala Dali"
 ]
 
 
-let joeKavalier: [String: Any] = [
+let joeKavalier: [String: String] = [
     "name": "Joe Kavalier",
-    "height": 39,
-    "soccerExperience": false,
+    "height": "39",
+    "soccerExperience": "No",
     "guardian": "Sam and Elaine Kavalier"
 ]
 
 
-let benFinkelstein: [String: Any] = [
+let benFinkelstein: [String: String] = [
     "name": "Ben Finkelstein",
-    "height": 44,
-    "soccerExperience": false,
+    "height": "44",
+    "soccerExperience": "No",
     "guardian": "Aaron and Jill Finkelstein"
 ]
 
 
-let diegoSoto: [String: Any] = [
+let diegoSoto: [String: String] = [
     "name": "Diego Soto",
-    "height": 41,
-    "soccerExperience": true,
+    "height": "41",
+    "soccerExperience": "Yes",
     "guardian": "Robin and Sarika Soto"
 ]
 
 
-let chloeAlaska: [String: Any] = [
+let chloeAlaska: [String: String] = [
     "name": "Chloe Alaska",
-    "height": 47,
-    "soccerExperience": false,
+    "height": "47",
+    "soccerExperience": "No",
     "guardian": "David and Jamie Alaska"
 ]
 
 
-let arnoldWillis: [String: Any] = [
+let arnoldWillis: [String: String] = [
     "name": "Arnold Willis",
-    "height": 43,
-    "soccerExperience": false,
+    "height": "43",
+    "soccerExperience": "No",
     "guardian": "Claire Willis"
 ]
 
 
-let phillipHelm: [String: Any] = [
+let phillipHelm: [String: String] = [
     "name": "Phillip Helm",
-    "height": 44,
-    "soccerExperience": true,
+    "height": "44",
+    "soccerExperience": "Yes",
     "guardian": "Thomas Helm and Eva Jones"
 ]
 
 
-let lesClay: [String: Any] = [
+let lesClay: [String: String] = [
     "name": "Les Clay",
-    "height": 42,
-    "soccerExperience": true,
+    "height": "42",
+    "soccerExperience": "Yes",
     "guardian": "Wynonna Brown"
 ]
 
 
-let herschelKrustofski: [String: Any] = [
+let herschelKrustofski: [String: String] = [
     "name": "Les Clay",
-    "height": 45,
-    "soccerExperience": true,
+    "height": "45",
+    "soccerExperience": "Yes",
     "guardian": "Hyman and Rachel Krustofski"
 ]
 
