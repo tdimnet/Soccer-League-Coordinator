59f226127d898edc71c188a7e38432e2853b3375
tdimnet <thomas.dimnet@gmail.com>
2018-04-07 08:21:44 +0200

    Initial Commit

---
+++ Soccer-League-Coordinator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Soccer-League-Coordinator
+//
+//  Created by Thomas Dimnet on 07/04/2018.
+//  Copyright © 2018 Thomas Dimnet. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
